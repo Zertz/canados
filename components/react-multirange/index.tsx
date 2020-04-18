042729eff5ef2f73ef7634a0c9908339ca954369
Pier-Luc Gendreau <pierluc@outlook.com>
2020-04-18 15:31:31 -0400

    Improve multirange input

--- components/react-multirange/index.tsx
+++ components/react-multirange/index.tsx
@@ -1,51 +1,87 @@
-import React, { useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 
 type MultirangeProps = {
   min: number;
   max: number;
   onChange: (values: [number, number]) => void;
-  values: [number?, number?];
+  values: [number, number];
 };
 
-function Multirange({
-  min = 0,
-  max = 100,
-  onChange,
-  values: [lowValue = min + (max - min) / 2, highValue = min + (max - min) / 2],
-}: MultirangeProps) {
-  const inputRef = useRef(null);
-  const ghostRef = useRef(null);
+function Multirange({ min = 0, max = 100, onChange, values }: MultirangeProps) {
+  const target = useRef<{ ref: "low" | "high" | null }>({ ref: null });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const ghostRef = useRef<HTMLInputElement>(null);
 
-  const handleChange = ({ target }) => {
-    const value = Number(target.value);
+  const [[lowValue, highValue], setNextValues] = useState<[number, number]>([
+    values[0],
+    values[1],
+  ]);
 
-    const low =
-      value < lowValue || value - lowValue <= highValue - value
-        ? value
-        : lowValue;
-
-    const high =
-      value > highValue || highValue - value < value - lowValue
-        ? value
-        : highValue;
-
-    if (lowValue === low && highValue === high) {
+  const updateStyles = (low, high) => {
+    if (!ghostRef.current) {
       return;
     }
 
-    // @ts-ignore
     ghostRef.current.style.setProperty(
       "--low",
       100 * ((low - min) / (max - min)) + 1 + "%"
     );
 
-    // @ts-ignore
     ghostRef.current.style.setProperty(
       "--high",
       100 * ((high - min) / (max - min)) - 1 + "%"
     );
+  };
 
-    onChange([low, high]);
+  useLayoutEffect(() => {
+    updateStyles(lowValue, highValue);
+  }, []);
+
+  const handleChange = (e) => {
+    const value = Number(e.target.value);
+
+    if (!target.current.ref) {
+      target.current.ref = value < lowValue ? "low" : "high";
+    }
+
+    const { low, high } =
+      target.current.ref === "low"
+        ? {
+            low: value,
+            high: value > highValue ? value : highValue,
+          }
+        : {
+            low: value < lowValue ? value : lowValue,
+            high: value,
+          };
+
+    if (lowValue === low && highValue === high) {
+      return;
+    }
+
+    updateStyles(low, high);
+
+    setNextValues([low, high]);
+  };
+
+  const handleMouseDown = (e) => {
+    if (lowValue === highValue) {
+      return;
+    }
+
+    const isOriginal = e.target.classList.contains("original");
+
+    target.current.ref = isOriginal ? "low" : "high";
+  };
+
+  const handleMouseUp = () => {
+    target.current.ref = null;
+
+    if (lowValue === values[0] && highValue === values[1]) {
+      return;
+    }
+
+    onChange([lowValue, highValue]);
   };
 
   return (
@@ -53,23 +89,25 @@ function Multirange({
       style={{ display: "flex", alignItems: "center", position: "relative" }}
     >
       <input
-        className={`multirange original${
-          lowValue > highValue ? " switched" : ""
-        }`}
+        className="multirange original"
         type="range"
         min={min}
         max={max}
         onChange={handleChange}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
         ref={inputRef}
         style={{ width: "100%" }}
         value={lowValue}
       />
       <input
-        className={`multirange ghost${lowValue < highValue ? " switched" : ""}`}
+        className="multirange ghost"
         type="range"
         min={min}
         max={max}
         onChange={handleChange}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
         ref={ghostRef}
         style={{ width: "100%" }}
         value={highValue}
