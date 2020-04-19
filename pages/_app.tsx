9010a6fd32e918e4876c85a3ea23736226a98508
Pier-Luc Gendreau <pierluc@outlook.com>
2020-04-19 14:11:07 -0400

    Use tornado emoji as favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,12 @@ export default function Canados({ Component, pageProps }) {
     <>
       <Head>
         <title>
-          🌪️ Interactive map of tornados in Canada and the United States
+          Interactive map of tornados in Canada and the United States
         </title>
+        <link
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌪️</text></svg>"
+          rel="icon"
+        />
       </Head>
       <Component {...pageProps} />
     </>
