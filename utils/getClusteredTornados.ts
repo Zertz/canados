62fb6bb7b43c1762166b972482fad768f3f5b696
Pier-Luc Gendreau <pierluc@outlook.com>
2020-05-03 14:31:31 -0400

    Remove splitting of large clusters

--- utils/getClusteredTornados.ts
+++ utils/getClusteredTornados.ts
@@ -12,7 +12,6 @@ export function getClusteredTornados({ tornados }: { tornados: Tornado[] }) {
 
   // 1. Compute geohash map starting at full geohash length
   // 2. Cluster tornados and, if the threshold is reached, start over with shorter geohash
-  // 3. Split largest clusters in half until threshold is reached
 
   let clusteredTornados: ClusteredTornado[] = [];
 
@@ -49,50 +48,6 @@ export function getClusteredTornados({ tornados }: { tornados: Tornado[] }) {
 
     if (clusteredTornados.length > 0) {
       break;
-    }
-  }
-
-  while (MAXIMUM_DISPLAYED_TORNADOS - clusteredTornados.length > 0) {
-    const unwindCount = MAXIMUM_DISPLAYED_TORNADOS - clusteredTornados.length;
-    const largestClusters: [number, number][] = [];
-
-    for (let i = 0; i < clusteredTornados.length; i += 1) {
-      const clusterLength = clusteredTornados[i].cluster.length;
-
-      if (clusterLength <= 1) {
-        continue;
-      }
-
-      if (
-        largestClusters.length === 0 ||
-        largestClusters.some(([, length]) => length < clusterLength)
-      ) {
-        largestClusters.push([i, clusterLength]);
-      }
-    }
-
-    if (largestClusters.length === 0) {
-      break;
-    }
-
-    if (largestClusters.length > unwindCount) {
-      largestClusters.sort(([, a], [, b]) => b - a).splice(unwindCount);
-    }
-
-    for (let i = 0; i < largestClusters.length; i += 1) {
-      const [clusterIndex] = largestClusters[i];
-      const tornado = clusteredTornados[clusterIndex];
-
-      const [
-        unclusteredTornado,
-        ...unclusteredTornados
-      ] = tornado.cluster.splice(0, Math.round(tornado.cluster.length / 2));
-
-      clusteredTornados.push({
-        ...unclusteredTornado,
-        cluster: unclusteredTornados,
-        clusterStats: getClusterStats([unclusteredTornado]),
-      });
     }
   }
 
