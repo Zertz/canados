b07e2813a86ef0ee7d18bd69d383ea9f364869bd
Pier-Luc Gendreau <pierluc@outlook.com>
2020-05-11 21:23:35 -0400

    Replace min/max in getBounds with built-in math functions

--- utils/getBounds.ts
+++ utils/getBounds.ts
@@ -2,24 +2,16 @@ export function getBounds(tornados: Tornado[]): Common.Bounds {
   const bounds = tornados.reduce(
     ([southWestBounds, northEastBounds], { coordinates_start }) => [
       [
-        min(southWestBounds[0], coordinates_start[0]),
-        min(southWestBounds[1], coordinates_start[1]),
+        Math.min(southWestBounds[0], coordinates_start[0]),
+        Math.min(southWestBounds[1], coordinates_start[1]),
       ],
       [
-        max(northEastBounds[0], coordinates_start[0]),
-        max(northEastBounds[1], coordinates_start[1]),
+        Math.max(northEastBounds[0], coordinates_start[0]),
+        Math.max(northEastBounds[1], coordinates_start[1]),
       ],
     ],
     [[], []]
   );
 
   return bounds as Common.Bounds;
-}
-
-function max(...values: any[]): number {
-  return values.reduce((acc, value) => (value > acc ? value : acc), -Infinity);
-}
-
-function min(...values: any[]): number {
-  return values.reduce((acc, value) => (value < acc ? value : acc), Infinity);
 }
