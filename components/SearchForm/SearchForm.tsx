fe3833c6facb67af2ac9b781cdf3dd456357a41b
Pier-Luc Gendreau <pierluc@outlook.com>
2020-05-19 21:03:52 -0400

    Test search form

--- components/SearchForm/SearchForm.tsx
+++ components/SearchForm/SearchForm.tsx
@@ -58,6 +58,7 @@ export default function SearchForm({ search }: Props) {
         </div>
         <button
           className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-r-md text-gray-700 bg-gray-50 hover:text-gray-500 hover:bg-white focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+          disabled={!value}
           type="submit"
         >
           Search
