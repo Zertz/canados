5023ef48a2a1347db05fb18018f3e807544fb328
Pier-Luc Gendreau <pierluc@outlook.com>
2021-12-30 10:26:56 -0500

    Update search input

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,3 +1,4 @@
+import SearchIcon from "@heroicons/react/solid/SearchIcon";
 import { useEffect, useState } from "react";
 import { useSearchParamState } from "../hooks/useSearchParamState";
 
@@ -32,23 +33,13 @@ export default function SearchForm({ search }: Props) {
       <label htmlFor="search" className="sr-only">
         Search tornados
       </label>
-      <div className="flex rounded-md shadow-md">
-        <div className="relative flex-grow focus-within:z-10">
+      <div className="mt-1 flex rounded-md shadow-sm">
+        <div className="relative flex items-stretch flex-grow focus-within:z-10">
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <svg
-              className="h-5 w-5 text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <SearchIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
           </div>
           <input
-            className="form-input block rounded-none rounded-l-md pl-10 transition ease-in-out duration-150 sm:text-sm sm:leading-5 w-full h-full"
+            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300"
             id="search"
             onChange={handleChange}
             placeholder="Toronto"
@@ -57,7 +48,7 @@ export default function SearchForm({ search }: Props) {
           />
         </div>
         <button
-          className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-r-md text-gray-700 bg-gray-50 hover:text-gray-500 hover:bg-white focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+          className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
           disabled={!value}
           type="submit"
         >
